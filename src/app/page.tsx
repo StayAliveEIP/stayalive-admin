ff02ff87c2b3a3a17745c057b70c89e9cf0f8d5e
FelixBuisson <felix.buisson@epitech.eu>
2024-01-31 09:37:06 +0000

    feat: add header to "home page"
    So I can push to prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import Image from "next/image";
-import {Input} from "@/components/ui/input";
+import {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {process.env.API_URL}
-      <Input />
+      <Menubar className="flex justify-between w-[1369px] m-auto mt-2">
+        <MenubarMenu>
+          <Link href="/signup">
+            <MenubarTrigger>Créer un compte Admin</MenubarTrigger>
+          </Link>
+          <Link href="/login">
+            <MenubarTrigger>Se connecter à un compte Admin</MenubarTrigger>
+          </Link>
+          <Link href="/admin">
+            <MenubarTrigger>Liste des Admins</MenubarTrigger>
+          </Link>
+          <Link href="/dashboard">
+            <MenubarTrigger>Dashboard Administrateur</MenubarTrigger>
+          </Link>
+        </MenubarMenu>
+      </Menubar>
     </main>
   );
 }
